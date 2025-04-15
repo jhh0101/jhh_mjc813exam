a4d097296407de5dab4693f37dd322053e0fc00b
jhh0101 <jhh010143@gmail.com>
2025-04-15 20:32:29 +0900

    exam

--- Exam/ExamProject/src/Exam.js
+++ Exam/ExamProject/src/Exam.js
@@ -1,3 +1,5 @@
+
+//로또 만들기
 let ranNum = 0;
 let ranIdxArr = [];
 while(ranIdxArr.length < 6){
@@ -9,7 +11,8 @@ while(ranIdxArr.length < 6){
 }
 console.log(ranIdxArr);
 
-let line = Number(prompt("숫자 입력"));
+//별찍기
+let line = Number(prompt("별 숫자 입력"));
 let result = "";
 for (let i = 1; i < line*2; i+=2) {
   for (let j = 1; j < ((line * 2) - i) / 2 ; j++) {
@@ -22,10 +25,45 @@ for (let i = 1; i < line*2; i+=2) {
 }
 console.log(result);
 
-
-
-//let name = prompt("입력하세요");
-//function reverseString(str) {
-//    return str.split("").reverse().join("");
+//let nSpace = 4;
+//let nStar = 1;
+//for(let nLine = 0; nLine<5; nLine++){
+//    let strLine = "";
+//    for(let nSp = 0; nSp < nSpace; nSp++){
+//        strLine += " ";
+//    }
+//    for(let nSt = 0; nSt < nStar; nSt++){
+//        strLine += "*";
+//    }
+//    console.log(strLine);
+//    nSpace--;
+//    nStar += 2;
 //}
-//console.log(reverseString(name));
+
+
+//게임 캐릭터 클래스 만들기
+class Wizard{
+    health = 0;
+    mana = 0;
+    armor = 0;
+    constructor(h, m, a){
+        this.health = h;
+        this.mana = m;
+        this.armor = a;
+    }
+    attack(){
+        console.log("파이어볼");
+    }
+}
+
+const x= new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+
+
+//거꾸로
+let name = prompt("거꾸로 문자 입력");
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString(name));
