99ac9b50a89118e7a5630f1bd10ab362c469ef96
jhh0101 <jhh010143@gmail.com>
2025-04-30 21:23:04 +0900

    Update Example.java

--- Exam/Study_java/src/ch01/verify/Example.java
+++ Exam/Study_java/src/ch01/verify/Example.java
@@ -3,8 +3,9 @@ package ch01.verify;
 public class Example { // 클래스
     // int a; => 멤버변수
     // static int c; => 클래스변수
-    public static void main/* => 메소드*/(String[] args /*매개변수*/ ) {
+    public static void main/* => 메소드 */(String[] args /* => 매개변수 */ ) {
         // int b; => 지역변수
+        // public => 전부 다 가능 , protected = > 다른 패키지에서 상속받은 클래스 + 같은 패키지, default => 같은 패키지, private => 같은 클래스
         System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
     }
 }
