6cba09714f23b945f20181f81ba18824c79c1781
jhh0101 <jhh010143@gmail.com>
2025-04-30 21:24:13 +0900

    Update Example.java

--- Exam/Study_java/src/ch01/verify/Example.java
+++ Exam/Study_java/src/ch01/verify/Example.java
@@ -5,7 +5,7 @@ public class Example { // 클래스
     // static int c; => 클래스변수
     public static void main/* => 메소드 */(String[] args /* => 매개변수 */ ) {
         // int b; => 지역변수
-        // 접근제한자 : (public => 전부 다 가능 , protected = > 다른 패키지에서 상속받은 클래스 + 같은 패키지, default => 같은 패키지, private => 같은 클래스)
+        // 접근제어자 : (public => 전부 다 가능 , protected = > 다른 패키지에서 상속받은 클래스 + 같은 패키지, default => 같은 패키지, private => 같은 클래스)
         System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
     }
 }
