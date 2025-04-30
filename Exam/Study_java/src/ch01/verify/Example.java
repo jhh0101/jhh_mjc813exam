a463703f8c897140fe12fc2afa1c4f5d74d490c1
jhh0101 <jhh010143@gmail.com>
2025-04-30 20:51:56 +0900

    variable
    기억할 것

--- Exam/Study_java/src/ch01/verify/Example.java
+++ Exam/Study_java/src/ch01/verify/Example.java
@@ -1,8 +1,10 @@
 package ch01.verify;
 
-public class Example {
-
-    public static void main(String[] args) {
+public class Example { // 클래스
+    // int a; => 멤버변수
+    // static int c; => 클래스변수
+    public static void main/* => 메소드*/(String[] args /*매개변수*/ ) {
+        // int b; => 지역변수
         System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
     }
 }
