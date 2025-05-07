966aba8b5ef25b4e821e09dcf3e81607c19dedbf
jhh0101 <jhh010143@gmail.com>
2025-05-07 20:55:59 +0900

    Revert "0507"
    This reverts commit 4c71ec0a571ca75686f22c2bea3340caab73f831.

--- Exam/Study_java/src/ch03/sec01/Exam04.java
+++
@@ -1,8 +0,0 @@
-package ch03.sec01;
-
-public class Exam04 {
-    public static void main(String[] args) {
-        int value = 356;
-        System.out.println(value / 100 * 100);
-    }
-}
--- eclips2/test1/src/test1/test1.java
+++ eclips2/test1/src/test1/test1.java
@@ -1,8 +1,9 @@
 package test1;
 
 public class test1 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
+
 }
