c8bc261c2d286f509e9b79cf18581bd68da9c389
Haehun <jhh010143@gmail.com>
2025-05-20 21:49:28 +0900

    123

--- Exam/Study_java/src/ch01/sec06/Hello.java
+++ Exam/Study_java/src/ch01/sec06/Hello.java
@@ -1,7 +1,24 @@
 package ch01.sec06;
 
 public class Hello {
+    static int a=8;
+
+    @Override
+    public String toString() {
+        return "Hello{" +
+                "a=" + a +
+                '}';
+    }
+
     public static void main(String[] args) {
+        Test test = new Test();
+        Hello hello = new Hello();
+        System.out.println("hello.toString() = " + hello);
+        test.setAge("15");
+        System.out.println("test.getAge() = " + test.getAge());
+
         System.out.println("Hello, Java");
+
+
     }
 }
---
+++ Exam/study_java/src/ch01/sec06/Test.java
@@ -0,0 +1,8 @@
+package ch01.sec06;
+
+import lombok.Data;
+
+@Data
+public class Test {
+    private String age;
+}
