426db905da728e8c8df806db0d74c9fa51f60dc2
Haehun <jhh010143@gmail.com>
2025-05-27 19:53:00 +0900

    myEmail
    API 이용하여 내 이메일로 메일 전송

---
+++ Exam/Calculator/src/main/java/com/mjc813/App.java
@@ -0,0 +1,15 @@
+package com.mjc813;
+
+import com.mjc813.create.CreateStar;
+import com.mjc813.library.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        
+
+    }
+}
