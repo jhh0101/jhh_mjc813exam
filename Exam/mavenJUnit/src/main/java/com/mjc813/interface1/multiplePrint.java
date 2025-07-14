07e302606be9c9b432bff47c00e3229945c01ff4
Haehun <jhh010143@gmail.com>
2025-07-14 09:13:50 +0900

    Update multiplePrint.java

--- Exam/mavenJUnit/src/main/java/com/mjc813/interface1/multiplePrint.java
+++ Exam/mavenJUnit/src/main/java/com/mjc813/interface1/multiplePrint.java
@@ -1,14 +1,14 @@
 package com.mjc813.interface1;
 
 class multiplePrint extends IteratorPrint implements Runnable{
-    private int a;
+    private final int a;
     multiplePrint(int a){
         this.a = a;
     }
 
     @Override
     public void run() {
-        int res = 0;
+        int res;
         for (int i = 1; i <= 100000; i++) {
             if(i % a == 0){
                 res = i;
