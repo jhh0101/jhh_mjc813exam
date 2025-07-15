a20420947fb0a454414658912bacece1f37d0032
Haehun <jhh010143@gmail.com>
2025-07-15 15:51:47 +0900

    model

--- Exam/sb_first/src/main/java/com/mjc813/sb_first/page/PageController.java
+++ Exam/sb_first/src/main/java/com/mjc813/sb_first/page/PageController.java
@@ -2,13 +2,15 @@ package com.mjc813.sb_first.page;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
     @GetMapping("/page")
-    public String page(){
+    public String page(Model model){
+        model.addAttribute("name","abcdefghijklnmopqrstuvwxyz");
         return "page";
     }
 }
