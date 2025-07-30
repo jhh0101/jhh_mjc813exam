c9fe57987d534cdb30c4c8c4aec1fc809c9ef1d5
Haehun <jhh010143@gmail.com>
2025-07-30 14:57:33 +0900

    Cinema 틀 만들기

---
+++ Exam/cinema_crud/src/main/java/com/mjc813/cinema_crud/cinema/dto/CinemaDto.java
@@ -0,0 +1,19 @@
+package com.mjc813.cinema_crud.cinema.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CinemaDto {
+    private Long id;
+    private String name;
+    private String genre_id;
+    private String playTime;
+    private String casts;
+    private String description;
+    private Integer restrictAge;
+}
